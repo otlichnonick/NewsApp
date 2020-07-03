ec618a855fd0b58ec21dbd7f7b41aaaa3161f527
Anton Agafonov <otlichnonick@yandex.ru>
2020-07-03 10:06:36 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Антон on 03.07.2020.
+//  Copyright © 2020 Anton Agafonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
